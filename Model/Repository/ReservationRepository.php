ac4e7a4018f8c25f437cbcd74a0b8e30fe177093
crespeau <ludovic3710@gmail.com>
2017-12-01 17:06:13 +0100

    add ReservationRepository

---
+++ Model/Repository/ReservationRepository.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: crespeau
+ * Date: 01/12/2017
+ * Time: 16:56
+ */
+
+class ReservationRepository
+{
+    public function insertReservation($entity){
+
+    }
+
+    public function selectReservation($entity, $id){
+
+    }
+
+    public function updateReservation($entity, $id){
+
+    }
+
+    public function deleteReservation($entity){
+
+    }
+}
