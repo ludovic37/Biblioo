c6658bad1c80d2777037c6a42a152793151254fa
Unknown <brunet.jph@gmail.com>
2017-12-01 15:55:05 +0100

    Auto stash before rebase of "master"

---
+++ Tools/DataManager/Repository.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jeep
+ * Date: 01/12/2017
+ * Time: 15:29
+ */
+
+class Repository implements RepositoryInterface
+{
+
+    public function createObject($entity)
+    {
+        // TODO: Implement createObject() method.
+    }
+
+    public function updateObject($entity, $id)
+    {
+        // TODO: Implement updateObject() method.
+    }
+
+    public function deleteObject($entity, $id)
+    {
+        // TODO: Implement deleteObject() method.
+    }
+
+    public function selectObect($entity)
+    {
+        // TODO: Implement selectObect() method.
+    }
+}
